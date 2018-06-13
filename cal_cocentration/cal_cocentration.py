3c09a5921c2a249b637107abcb7e8589f693ead0
LAPTOP-UBIOPS8M\ei-tk <ei.tktkwk.ud.f.603@gmai.com>
2018-06-13 10:14:57 +0900

    濃度計算_履歴機能の追加

--- cal_cocentration/cal_cocentration.py
+++ cal_cocentration/cal_cocentration.py
@@ -1,13 +1,26 @@
 import tkinter as tk
 import tkinter.messagebox as msg
+import pandas as pd
 
 #計算して値をメッセージボックスで返す
 def result():
     pesticide = (int_total.get() * int_concentration.get()) / 100
     if pesticide > 0:
         msg.showinfo('結果', '農薬を' + str(pesticide) + 'g用意してください')
+        df = pd.DataFrame([[str(int_total.get()), str(int_concentration.get()), str(pesticide)]],
+                          columns=['total', 'concentration', 'pesticide'])
+        df.to_csv("data.csv", mode='a', header=False)
     else:
         msg.showinfo('結果', '入力値を確認してください')
+
+def show():
+    sh = pd.read_csv("data.csv", names=['mL', '%', 'g'], usecols=[1, 2, 3])
+    root = tk.Tk()
+    root.title("計算履歴")
+    root_canvas = tk.Canvas(root, width=250, height=150, bd=0, highlightthickness=0)
+    l_show = tk.Label(root, text=sh).pack()
+    root_canvas.pack()
+
 
 #ウィンドウの設定
 base = tk.Tk()
@@ -35,9 +48,12 @@ tx_concentration.place(x=50, y=60)
 l_par = tk.Label(base, text="%")
 l_par.place(x=180, y=60)
 
-#ボタンの生成
+#計算ボタンの生成
 btn_cal = tk.Button(base, text="計算", command=result)
 btn_cal.place(x=90, y=90)
+
+btn_show = tk.Button(base, text="履歴", command=show)
+btn_show.place(x=90, y=120)
 
 base.mainloop()
 

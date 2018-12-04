1f4c8a20fadaab8f0eec401d808194aaf7732c19
五十嵐瑛 <ei.tktcwk.ud.f.603@gmail.com>
2018-12-04 20:35:26 +0900

    コメントの追加

--- weather/weather.py
+++ weather/weather.py
@@ -2,7 +2,10 @@
 import requests
 import pprint
 
+# APIを叩いて東京（1300010）の天気情報を取得
 api_url = 'http://weather.livedoor.com/forecast/webservice/json/v1'
 payload = {'city':'130010'}
 weather_data = requests.get(api_url, params=payload).json()
+
+# 当日の天気を表示
 print(weather_data['forecasts'][0]['dateLabel'] + 'の天気は、' + weather_data['forecasts'][0]['telop'])
